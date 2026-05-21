81f2c389d65e2c7e82c6c3650b628427b5e1c8c2
vishavjeet1999 <vishavjeetsharma88@gmail.com>
2026-05-21 09:23:44 +0530

    changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -253,7 +253,7 @@ function Hero({ liveVisitors, billasJoined }: { liveVisitors: number; billasJoin
             </Button>
           </motion.div>
 
-          <motion.div
+          {/* <motion.div
             initial={{ opacity: 0, y: 24 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.7, duration: 0.6 }}
@@ -273,7 +273,7 @@ function Hero({ liveVisitors, billasJoined }: { liveVisitors: number; billasJoin
                 </div>
               </div>
             ))}
-          </motion.div>
+          </motion.div> */}
 
         </div>
 
